99de8766290d54333dac579e2166e1379c5ce5e6
utopian <utopian72@naver.com>
2021-04-12 14:10:01 +0900

    util 추가

---
+++ com.study.core.utility/ExcelToDataTableWithOLE.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace com.study.core.utility
+{
+
+    public interface IExcelToDataTable
+    {
+        DataTable convertTo(string filePath);
+    }
+
+    public class ExcelToDataTableWithOLE: IExcelToDataTable
+    {
+        private const string oleConStringFormat = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+
+        public int  TargetSheetIndex{ get; set; } =0;
+
+        public DataTable convertTo(string filePath)
+        {
+            DataTable dt = new DataTable();
+            string conString = string.Format(oleConStringFormat, filePath);
+
+            using (OleDbConnection connExcel = new OleDbConnection(conString))
+            {
+                using (OleDbCommand cmdExcel = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                    {
+                        cmdExcel.Connection = connExcel;
+
+                        //Get the name of First Sheet.
+                        connExcel.Open();
+                        DataTable dtExcelSchema;
+                        dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                        string sheetName = dtExcelSchema.Rows[TargetSheetIndex]["TABLE_NAME"].ToString();
+                        connExcel.Close();
+
+                        //Read Data from First Sheet.
+                        connExcel.Open();
+                        cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                        odaExcel.SelectCommand = cmdExcel;
+                        odaExcel.Fill(dt);
+                        connExcel.Close();
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ com.study.core.utility/ToExcelWithOfficeOpenXml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
+using OfficeOpenXml;
+using System.Linq;
+using OfficeOpenXml.Style;
+using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace com.study.core.utility
+{
+    public interface IToExcel
+    {
+        MemoryStream convertToExcel<T>(List<T> subscribers);
+    }
+    public class ToExcelWithOfficeOpenXml : IToExcel
+    {
+        public string DefaultSheetName { get; set; } = "Result";
+        public string DataPosition { get; set; } = "A2";
+        public bool DrawingHeaderBoarder { get; set; } = true;
+
+        public MemoryStream convertToExcel <T>(List<T> subscribers )
+        {
+            var stream = new System.IO.MemoryStream();
+            using (ExcelPackage package = new ExcelPackage(stream))
+            {
+                ExcelWorksheet ws = package.Workbook.Worksheets.Add(DefaultSheetName);
+
+                var t = typeof(T);
+                var Headings = t.GetProperties();
+                for (int i = 0; i < Headings.Count(); i++)
+                {
+                    ws.Cells[1, i + 1].Value = Headings[i].Name;
+                }
+
+                if (subscribers.Count() > 0)
+                {
+                    ws.Cells[DataPosition].LoadFromCollection(subscribers);
+                }
+
+                if (DrawingHeaderBoarder)
+                {
+                    using (ExcelRange rng = ws.Cells["A1:BZ1"])
+                    {
+                        rng.Style.Font.Bold = true;
+                        rng.Style.Fill.PatternType = ExcelFillStyle.Solid;                      //Set Pattern for the background to Solid
+                        rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));  //Set color to dark blue
+                        rng.Style.Font.Color.SetColor(Color.White);
+                    }
+                }
+                package.Save();
+            }
+            return stream;
+        }
+
+    }
+}

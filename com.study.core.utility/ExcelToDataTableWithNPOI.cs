7a24f0fc813ebd952fe0d1de420c3a229c4006ac
utopian <utopian72@naver.com>
2021-05-06 15:53:31 +0900

    친

---
+++ com.study.core.utility/ExcelToDataTableWithNPOI.cs
@@ -0,0 +1,99 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace com.study.core.utility
+{
+    public class ExcelToDataTableWithNPOI: IExcelToDataTable
+    {
+        private static int _shteetindex = 0;
+        public DataTable convertTo(string filePath, int sheetindex = 0)
+        {
+            _shteetindex = sheetindex;
+
+            return GetRequestsDataFromExcel(filePath);
+        }
+
+        private static ISheet GetFileStream(string fullFilePath)
+        {
+            var fileExtension = Path.GetExtension(fullFilePath);
+            string sheetName;
+            ISheet sheet = null;
+            switch (fileExtension)
+            {
+                case ".xlsx":
+                    using (var fs = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        var wb = new XSSFWorkbook(fs);
+                        sheetName = wb.GetSheetAt(_shteetindex).SheetName;
+                        sheet = (XSSFSheet)wb.GetSheet(sheetName);
+                    }
+                    break;
+                case ".xls":
+                    using (var fs = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        var wb = new HSSFWorkbook(fs);
+                        sheetName = wb.GetSheetAt(_shteetindex).SheetName;
+                        sheet = (HSSFSheet)wb.GetSheet(sheetName);
+                    }
+                    break;
+            }
+            return sheet;
+        }
+
+        private static DataTable GetRequestsDataFromExcel(string fullFilePath)
+        {
+            try
+            {
+                var sh = GetFileStream(fullFilePath);
+                var dtExcelTable = new DataTable();
+                dtExcelTable.Rows.Clear();
+                dtExcelTable.Columns.Clear();
+                var headerRow = sh.GetRow(0);
+                int colCount = headerRow.LastCellNum;
+                for (var c = 0; c < colCount; c++)
+                    dtExcelTable.Columns.Add(headerRow.GetCell(c).ToString());
+                var i = 1;
+                var currentRow = sh.GetRow(i);
+                while (currentRow != null)
+                {
+                    var dr = dtExcelTable.NewRow();
+                    for (var j = 0; j < currentRow.Cells.Count; j++)
+                    {
+                        var cell = currentRow.GetCell(j);
+
+                        if (cell != null)
+                            switch (cell.CellType)
+                            {
+                                case CellType.Numeric:
+                                    dr[j] = DateUtil.IsCellDateFormatted(cell)
+                                        ? cell.DateCellValue.ToString(CultureInfo.InvariantCulture)
+                                        : cell.NumericCellValue.ToString(CultureInfo.InvariantCulture);
+                                    break;
+                                case CellType.String:
+                                    dr[j] = cell.StringCellValue;
+                                    break;
+                                case CellType.Blank:
+                                    dr[j] = string.Empty;
+                                    break;
+                            }
+                    }
+                    dtExcelTable.Rows.Add(dr);
+                    i++;
+                    currentRow = sh.GetRow(i);
+                }
+                return dtExcelTable;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}

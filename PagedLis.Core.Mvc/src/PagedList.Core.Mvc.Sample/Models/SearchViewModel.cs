57649b7c676410f8caa4438fe9186e3482c0165c
utopian <utopian72@naver.com>
2021-05-06 14:04:59 +0900

    aaa

--- PagedLis.Core.Mvc/src/PagedList.Core.Mvc.Sample/Models/SearchViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using PagedList.Core.Mvc.Sample.Search;
-
-namespace PagedList.Core.Mvc.Sample.Models
-{
-    public class SearchViewModel
-    {
-        public SearchResult SearchResult { get; set; }
-    }
-}
